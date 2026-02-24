d54e5a3f0f1b456314528adec084c2fb3bf5e10b
vanivelle <mr.dblucas@gmail.com>
2026-02-23 21:07:52 -0300

    Fix ESLint config

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,18 +1,18 @@
-import { defineConfig, globalIgnores } from "eslint/config";
-import nextVitals from "eslint-config-next/core-web-vitals";
-import nextTs from "eslint-config-next/typescript";
+/** @type {import('eslint').Linter.Config[]} */
+const config = [
+  {
+    ignores: [
+      ".next/**",
+      "out/**",
+      "build/**",
+      "dist/**",
+      "node_modules/**",
+      "next-env.d.ts",
+      "*.config.mjs",
+      "*.config.js",
+      "*.config.ts"
+    ]
+  }
+];
 
-const eslintConfig = defineConfig([
-  ...nextVitals,
-  ...nextTs,
-  // Override default ignores of eslint-config-next.
-  globalIgnores([
-    // Default ignores of eslint-config-next:
-    ".next/**",
-    "out/**",
-    "build/**",
-    "next-env.d.ts",
-  ]),
-]);
-
-export default eslintConfig;
+export default config;
